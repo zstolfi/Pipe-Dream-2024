8190b71f6d532e6bde9956c634b57c85e98d1dd4
TheZacher5645 <zander5645@yahoo.com>
2024-02-07 18:41:31 -0600

    Move member functions "outside" the class

--- epsiode 1/scripts/MIDI.lua
+++ epsiode 1/scripts/MIDI.lua
@@ -1,11 +1,8 @@
 Util = require(workspace.scripts.Util);
 
 local MIDI = {
-
 	Parser = {
-		-- Unusable table.clone function, I'll have to
-		-- rewrite my own 'deep copy' version soon ...
-		new = function(self) return table.clone(self); end;
+		new = function(self) return Util.deepCopy(self); end;
 
 		result = {
 			header = {};
@@ -15,32 +12,32 @@ local MIDI = {
 		i = 0;
 		done = false;
 		error = nil;
-
-		parse = function(self, base64Str) --> expected MIDI table
-			local bytes = Util.parseBase64(base64Str);
-
-			-- Check the file's 'magic bytes' before continuing.
-			if bytes:sub(1,4) ~= "MThd" then
-				return nil, "Input file is not a MIDI file";
-			end
-
-			self:parseHeader(bytes);
-			repeat
-				self:parseTrack(bytes);
-			until (self.done);
-			
-			return self.result;
-		end;
-
-		parseHeader = function(self, bytes)
-			table.insert(self.result.header, "Header Data!");
-		end;
-
-		parseTrack = function(self, bytes)
-			table.insert(self.result.tracks[1], "Track Data!");
-			self.done = true;
-		end;
-	},
+	};
 };
+
+function MIDI.Parser.parse(self, base64Str) --> expected MIDI table
+	local bytes = Util.parseBase64(base64Str);
+
+	-- Check the file's 'magic bytes' before continuing.
+	if bytes:sub(1,4) ~= "MThd" then
+		return nil, "Input file is not a MIDI file";
+	end
+
+	self:parseHeader(bytes);
+	repeat
+		self:parseTrack(bytes);
+	until (self.done);
+	
+	return self.result;
+end
+
+function MIDI.Parser.parseHeader(self, bytes)
+	table.insert(self.result.header, "Header Data!");
+end
+
+function MIDI.Parser.parseTrack(self, bytes)
+	table.insert(self.result.tracks[1], "Track Data!");
+	self.done = true;
+end
 
 return MIDI;
--- epsiode 1/scripts/Util.lua
+++ epsiode 1/scripts/Util.lua
@@ -1,64 +1,74 @@
-local Util = {
-	parseBase64 = function(b64) --> binary string
-		local Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		..               "abcdefghijklmnopqrstuvwxyz"
-		..               "0123456789+/";
-		local Pad = "=";
-		local isDigit = function(c) return Alphabet:find(c) ~= nil; end;
-		local isPad   = function(c) return c == Pad; end;
-		local digitOf = function(c)
-			return (c == Pad) and 0 or Alphabet:find(c) - 1;
-		end;
+local Util = {};
 
-		-- Check input size.
-		if (#b64 % 4) ~= 0 or #b64 == 0 then
-			return nil, "Unexpected b64 length";
-		end
+--[[ Class Functions ]]-----------------------------------------------------
+function Util.deepCopy(T)
+	local result = {};
+	for i,v in pairs(T) do
+		result[i] = (type(v) == "table") and Util.deepCopy(v) or v;
+	end
+	return result;
+end
 
-		-- Store each group of 4 bytes in a table.
-		local tuples = {};
-		for i=1, #b64, 4 do
-			tuples[#tuples + 1] = b64:sub(i, i+3);
-		end
+--[[ Binary I/O Functions ]]------------------------------------------------
+function Util.parseBase64(b64) --> binary string
+	local Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	..               "abcdefghijklmnopqrstuvwxyz"
+	..               "0123456789+/";
+	local Pad = "=";
+	local isDigit = function(c) return Alphabet:find(c) ~= nil; end;
+	local isPad   = function(c) return c == Pad; end;
+	local digitOf = function(c)
+		return (c == Pad) and 0 or Alphabet:find(c) - 1;
+	end;
 
-		-- Check for correct digits.
-		for i=1, #tuples do
-			local isLast = i == #tuples;
-			for j=1, (isLast and 2 or 4) do
-				if not Alphabet:find(tuples[i]:sub(j,j)) then
-					return nil, "Invalid b64 digit";
-				end
+	-- Check input size.
+	if (#b64 % 4) ~= 0 or #b64 == 0 then
+		return nil, "Unexpected b64 length";
+	end
+
+	-- Store each group of 4 bytes in a table.
+	local tuples = {};
+	for i=1, #b64, 4 do
+		tuples[#tuples + 1] = b64:sub(i, i+3);
+	end
+
+	-- Check for correct digits.
+	for i=1, #tuples do
+		local isLast = i == #tuples;
+		for j=1, (isLast and 2 or 4) do
+			if not Alphabet:find(tuples[i]:sub(j,j)) then
+				return nil, "Invalid b64 digit";
 			end
 		end
+	end
 
-		local p, q = unpack(tuples[#tuples]:sub(3,4):split(""));
-		if  not (isDigit(p) and isDigit(q))
-		and not (isDigit(p) and isPad  (q))
-		and not (isPad  (p) and isPad  (q)) then
-			return nil, "Invalid b64 padding";
-		end
+	local p, q = unpack(tuples[#tuples]:sub(3,4):split(""));
+	if  not (isDigit(p) and isDigit(q))
+	and not (isDigit(p) and isPad  (q))
+	and not (isPad  (p) and isPad  (q)) then
+		return nil, "Invalid b64 padding";
+	end
 
-		-- Transform into bytes.
-		local result = "";
-		for i=1, #tuples do
-			local c1, c2, c3, c4 = unpack(tuples[i]:split(""))
-			result = result .. string.char(
-				(digitOf(c1)* 4 + digitOf(c2)//16) % 256,
-				(digitOf(c2)*16 + digitOf(c3)// 4) % 256,
-				(digitOf(c3)*64 + digitOf(c4)// 1) % 256
-			);
-		end
+	-- Transform into bytes.
+	local result = "";
+	for i=1, #tuples do
+		local c1, c2, c3, c4 = unpack(tuples[i]:split(""))
+		result = result .. string.char(
+			(digitOf(c1)* 4 + digitOf(c2)//16) % 256,
+			(digitOf(c2)*16 + digitOf(c3)// 4) % 256,
+			(digitOf(c3)*64 + digitOf(c4)// 1) % 256
+		);
+	end
 
-		return result;
-	end,
+	return result;
+end
 
-	printBytes = function(bytes)
-		local hex = function(x) return ("0123456789abcdef"):sub(x+1,x+1); end
-		local line = "";
-		for i=1, #bytes do
-			print(hex(bytes:byte(i,i)//16) .. hex(bytes:byte(i,i)%16));
-		end
-	end,
-};
+function Util.printBytes(bytes)
+	local hex = function(x) return ("0123456789abcdef"):sub(x+1,x+1); end
+	local line = "";
+	for i=1, #bytes do
+		print(hex(bytes:byte(i,i)//16) .. hex(bytes:byte(i,i)%16));
+	end
+end
 
 return Util;
